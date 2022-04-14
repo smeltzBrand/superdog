f6a6ee8603943f38c10008f6ab084c8914d60e0b
smeltzBrand <101213259+smeltzBrand@users.noreply.github.com>
2022-04-14 14:13:50 -0400

    Added all data

--- js/site.js
+++ js/site.js
@@ -80,7 +80,7 @@ function buildDropDown() {
     //get the template node
     let ddItem = document.importNode(ddTemplate.content, true);
 
-    // add the all item to the dropdown
+    // add the 'All' items to the dropdown
     let ddLink = ddItem.querySelector("a");
     ddLink.setAttribute("data-city", "All");
     ddLink.textContent = "All";
@@ -98,7 +98,7 @@ function buildDropDown() {
 
         let ddItem = document.importNode(ddTemplate.content, true);
 
-        // add the all item to the dropdown
+        // add the other items to the dropdown
         let ddLink = ddItem.querySelector("a");
         ddLink.setAttribute("data-city", distinctCities[i]);
         ddLink.textContent = distinctCities[i];
@@ -112,6 +112,9 @@ function buildDropDown() {
 
     //show stats for all events
     displayStats(curEvents);
+
+    //show data for all events
+    displayEventData(curEvents);
 
 }
 
@@ -132,12 +135,12 @@ function getEventData(element) {
         });
     }
 
-        //set the header
+    //set the header
     let statsHeader = document.getElementById("statsHeader")
     statsHeader.innerHTML = `Stats for ${city}`;
 
-        //Call a function to display the stats
-        displayStats(filteredEvents);
+    //Call a function to display the stats
+    displayStats(filteredEvents);
 }
 
 //pull the events from local storage
@@ -147,7 +150,7 @@ function getEvents() {
     if (curEvents === null) {
         curEvents = events;
         localStorage.setItem("eventData", JSON.stringify(curEvents));
-    } 
+    }
 
 
     return curEvents;
@@ -163,10 +166,10 @@ function displayStats(filteredEvents) {
     let currentAttendance = 0;
 
     for (let i = 0; i < filteredEvents.length; i++) {
-        
+
         currentAttendance = filteredEvents[i].attendance;
         total += currentAttendance;
-        
+
         if (most < currentAttendance) {
             most = currentAttendance;
         }
@@ -183,10 +186,74 @@ function displayStats(filteredEvents) {
     document.getElementById("most").innerHTML = most.toLocaleString();
     document.getElementById("least").innerHTML = least.toLocaleString();
     document.getElementById("average").innerHTML = average.toLocaleString(
-        undefined,{
+        undefined, {
             minmumFractionDigits: 0,
             maximumFractionDigits: 0
         }
     );
 
+}
+
+// this function displays all of the event data in a 
+//grid on bottom of page
+function displayEventData(curEvents) {
+    let template = document.getElementById("eventData-template");
+    let eventBody = document.getElementById("eventBody");
+    //clear the table to any previous data
+    eventBody.innerHTML = "";
+
+    // loop over all the eventdata and write a row for each event
+    // to the eventBody element.
+    for (let i = 0; i < curEvents.length; i++) {
+        let eventRow = document.importNode(template.content, true);
+
+        //grab only the columns from the template
+        //creates an array of the columns in the template
+        let eventCols = eventRow.querySelectorAll("td");
+
+        eventCols[0].textContent = curEvents[i].event;
+        eventCols[1].textContent = curEvents[i].city;
+        eventCols[2].textContent = curEvents[i].state;
+        eventCols[3].textContent = curEvents[i].attendance;
+        eventCols[4].textContent = new Date(curEvents[i].date).toLocaleDateString();
+
+        eventBody.appendChild(eventRow);
+    }
+
+}
+
+function saveEventData() {
+    //get all of the course data from localstorage
+    let curEvents = getEvents();
+
+    let eventObj = {
+        event: "name",
+        city: "city",
+        state: "state",
+        attendance: 0,
+        date: "01/01/2000"
+    }
+
+    //get the values from the form
+    eventObj.event = document.getElementById("newEventName").value;
+    eventObj.city = document.getElementById("newEventCity").value;
+
+    // get values from the dropdown
+    let stateSel = document.getElementById("newEventState");
+    eventObj.state = stateSel.options[stateSel.selectedIndex].text;
+    
+    // turn the input into a number
+    let attendanceNbr = parseInt(document.getElementById("newEventAttendance").value,10);
+    eventObj.attendance = attendanceNbr;
+
+     //format the date before saving
+    let eventDate = document.getElementById("newEventDate").value;
+    let eventDateFormatted = `${eventDate} 00:00`;
+    eventObj.date = new Date(eventDateFormatted).toLocaleDateString();
+   
+    //save
+    curEvents.push(eventObj);
+    localStorage.setItem("eventData", JSON.stringify(curEvents));
+
+    buildDropDown();
 }
